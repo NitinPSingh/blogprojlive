9cd27f927a2b7ed6dba51c29f5b54bbf7c056b3f
NitinPSingh <71833171+NitinPSingh@users.noreply.github.com>
2021-03-21 13:07:08 +0530

    Update settings.py

--- blogproj/settings.py
+++ blogproj/settings.py
@@ -80,15 +80,10 @@ WSGI_APPLICATION = 'blogproj.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'blogapp_db',
-        'USER':'postgres',
-        'PASSWORD':'1234',
-        'HOST':'localhost',
-        'PORT':'5432'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
