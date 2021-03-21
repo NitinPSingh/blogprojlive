046d46f4d51e4d3fe7b32ab99397d059842dc26e
NitinPSingh <71833171+NitinPSingh@users.noreply.github.com>
2021-03-21 12:51:24 +0530

    Update settings.py

--- blogproj/settings.py
+++ blogproj/settings.py
@@ -135,7 +135,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL='/static/'
 STATICFILES_DIRS = [
     STATIC_DIR,
